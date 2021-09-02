b2f60d336156388bc0dcd66d10ad38062eb26fc5
Sliver Ray <sonrin746@gmail.com>
2021-09-02 08:08:36 +0700

    12

---
+++ JS/js09.js
@@ -0,0 +1,12 @@
+function Add() {
+    var n = document.getElementById("input").value;
+    if (n == "") {
+        document.getElementById("alert").innerHTML = "Vui lòng nhập chuỗi!";
+    }
+    else {
+        str1 = document.getElementById("str1").value;
+        str2 = document.getElementById("str2").value;
+        n = n.replace(str1 , str2);
+        document.getElementById("input").value = "XX";
+    }
+}
