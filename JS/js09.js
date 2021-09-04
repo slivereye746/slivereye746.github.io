b8d468fa4c10ef6c67c89153e636449f6a7fd275
Sliver Ray <sonrin746@gmail.com>
2021-09-04 09:41:57 +0700

    09

--- JS/js09.js
+++ JS/js09.js
@@ -3,10 +3,21 @@ function Add() {
     if (n == "") {
         document.getElementById("alert").innerHTML = "Vui lòng nhập chuỗi!";
     }
-    else {
-        //str1 = document.getElementById("str1").value;
-        //str2 = document.getElementById("str2").value;
-        n = n.replace(str1 , str2);
-        document.getElementById("input").value = "XX";
+    else {        
+        document.getElementById("alert").innerHTML = "";
+        //var str1 = document.getElementById("str1").value;
+        //var str2 = document.getElementById("str2").value;
+        if (document.getElementById("str1").value == "") {
+            document.getElementById("str1").placeholder = "Vui lòng chọn chuỗi bị thay thế!";
+        }
+        else if (document.getElementById("str2").value == "") {
+            document.getElementById("str2").placeholder = "Vui lòng chọn chuỗi  thay thế!";
+        }
+        else {
+            var n = n.replaceAll(document.getElementById("str1").value, document.getElementById("str2").value);
+            document.getElementById("input").value = n;
+        }
+        //document.getElementById("input").value = "XX";
+        
     }
-}
+} 
