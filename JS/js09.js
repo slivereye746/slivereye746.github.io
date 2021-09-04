656d2447ef8c68943845c3036dcdbb3d82c7b229
Sliver Ray <sonrin746@gmail.com>
2021-09-04 14:19:05 +0700

    x

--- JS/js09.js
+++ JS/js09.js
@@ -1,12 +1,9 @@
 function Add() {
     var n = document.getElementById("input").value;
     if (n == "") {
-        document.getElementById("alert").innerHTML = "Vui lòng nhập chuỗi!";
+        document.getElementById("input").placeholder = "Vui lòng nhập chuỗi!";
     }
     else {        
-        document.getElementById("alert").innerHTML = "";
-        //var str1 = document.getElementById("str1").value;
-        //var str2 = document.getElementById("str2").value;
         if (document.getElementById("str1").value == "") {
             document.getElementById("str1").placeholder = "Vui lòng chọn chuỗi bị thay thế!";
         }
