e930c91fb84ca945800a35657585a9b6719a8f0f
Sliver Ray <sonrin746@gmail.com>
2021-09-27 06:59:56 +0700

    Update js11.js

--- JS/js11.js
+++ JS/js11.js
@@ -1,5 +1,28 @@
 function Add(){
     var str = document.getElementById("input").value;
-    let leng = str.length;
-    document.getElementById("rs").innerHTML = "Character: "+ leng.toString();
+    if (str != "") {
+        //
+        var str2 = str.replace(/\s+/g, " ");
+        str2 = str2.trim();
+        const arr = str2.split(" ");
+        //
+        let leng = str.length;
+        //
+        document.getElementById("rs").innerHTML = "Character: "+ leng.toString() + "<br>" 
+                                                + "Words: " + arr.length + "<br>";
+        //
+        var countA = str.length() - string.replaceAll("g","").length();
+        if (countA > 0) {
+            document.getElementById("rs").innerHTML += "Character 'A': " + countA.toString();
+        }
+        const stringArr = split
+        const lietke = []; 
+        
+
+        
+    }
+    else {
+        document.getElementById("rs").innerHTML = "Vui lòng nhập chuỗi!";
+    }
+    
 }
